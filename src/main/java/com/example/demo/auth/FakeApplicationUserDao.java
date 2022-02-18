f8571bd2fc6d472bc880b8935e1e93f43d6c1dba
Daniel Soares <danielsoares321@gmail.com>
2022-02-18 03:48:32 -0300

    fixing

--- src/main/java/com/example/demo/auth/FakeApplicationUserDao.java
+++ src/main/java/com/example/demo/auth/FakeApplicationUserDao.java
@@ -5,10 +5,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Repository;
 
 import com.example.demo.security.ApplicationUserRole;
 import com.google.common.collect.Lists;
 
+@Repository("fake")
 public class FakeApplicationUserDao implements ApplicationUserDao{
 	
 	private final PasswordEncoder passwordEncoder;	
--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -5,16 +5,16 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.example.demo.auth.ApplicationUserService;
 
 @Configuration
 @EnableWebSecurity
@@ -24,10 +24,12 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 	
 	
 	private final PasswordEncoder passwordEncoder;
+	private final ApplicationUserService applicationUserService;
 	
 	@Autowired
-	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder,ApplicationUserService applicationUserService) {
 		this.passwordEncoder = passwordEncoder;
+		this.applicationUserService = applicationUserService;
 	}
 // The order of antMatchers matters!
 	@Override
@@ -59,34 +61,21 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter{
 		.deleteCookies("JSESSIONID", "remember-me")
 		.logoutSuccessUrl("/login");  
 	}
-
-	@Override
-	@Bean
-	protected UserDetailsService userDetailsService() {
-		UserDetails annaSmithUser = User.builder()
-		.username("annasmith")
-		.password(passwordEncoder.encode("password"))
-		//.roles(ApplicationUserRole.STUDENT.name())
-		.authorities(ApplicationUserRole.STUDENT.getGrantedAuthorities())
-		.build();
-		
-		UserDetails lindaUser = User.builder()
-				.username("linda")
-				.password(passwordEncoder.encode("password123"))
-		//		.roles(ApplicationUserRole.ADMIN.name())
-				.authorities(ApplicationUserRole.ADMIN.getGrantedAuthorities())
-				.build();
-		
-		UserDetails tomUser = User.builder()
-				.username("tom")
-				.password(passwordEncoder.encode("password123"))
-		//		.roles(ApplicationUserRole.ADMINTRAINEE.name())
-				.authorities(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities())
-				.build();
-		
-		return new InMemoryUserDetailsManager(annaSmithUser, lindaUser, tomUser);
-	}
 	
+	
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(daoAuthenticationProvider());
+	}
+	@Bean
+	public DaoAuthenticationProvider daoAuthenticationProvider() { 
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(passwordEncoder); //allows password to be decoded
+		provider.setUserDetailsService(applicationUserService);
+		return provider;
+		
+	}
 	
 
 }
