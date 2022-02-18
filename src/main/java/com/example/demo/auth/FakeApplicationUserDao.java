c0dc5d972ed3f067078733c839c85c52f5db44c7
Daniel Soares <danielsoares321@gmail.com>
2022-02-17 22:00:55 -0300

    Database authentication, aFakeAppliationUserDaoService

---
+++ src/main/java/com/example/demo/auth/FakeApplicationUserDao.java
@@ -0,0 +1,43 @@
+package com.example.demo.auth;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.example.demo.security.ApplicationUserRole;
+import com.google.common.collect.Lists;
+
+public class FakeApplicationUserDao implements ApplicationUserDao{
+	
+	private final PasswordEncoder passwordEncoder;	
+	
+	@Autowired
+	public FakeApplicationUserDao(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+
+	@Override
+	public Optional<ApplicationUser> selectApplicationUserByUserName(String username) {
+		return getApplicationUsers().stream().filter(applicationUser -> username.equals(applicationUser.getUsername())).findFirst();
+	}
+	
+	private List<ApplicationUser> getApplicationUsers(){
+		List<ApplicationUser> applicationUsers = Lists.newArrayList(
+				new ApplicationUser(ApplicationUserRole.STUDENT.getGrantedAuthorities(),
+						passwordEncoder.encode("password"), 
+						"annasmith", true, true, true, true),
+				
+				new ApplicationUser(ApplicationUserRole.ADMIN.getGrantedAuthorities(),
+						passwordEncoder.encode("password"), 
+						"linda", true, true, true, true),
+				
+				new ApplicationUser(ApplicationUserRole.ADMINTRAINEE.getGrantedAuthorities(),
+						passwordEncoder.encode("password"), 
+						"tom", true, true, true, true));
+		return applicationUsers;
+		
+	}
+
+}
